bc8ad04c0a2b06de470b00cd26ac825dd388303f
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-03-09 16:12:30 -0300

    first

---
+++ basico/exPalindromo.js
@@ -0,0 +1,8 @@
+const input = require('prompt-sync')();
+
+let n1 = parseInt(input('digite um numero :'));
+let n2= parseInt(input('digite outro  numero :'));
+
+console.log(`soma de ${n1} com ${n2} = ${n1 + n2}`);
+
+
