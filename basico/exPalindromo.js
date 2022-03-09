62e72ee22247fd87852ccdd0c0ceacc48046983d
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-03-09 16:54:24 -0300

    pal4ffff43

--- basico/exPalindromo.js
+++ basico/exPalindromo.js
@@ -24,6 +24,6 @@ if(isPalindromo) console.log(`A palavra ${palavra} é um palíndromo !`);
 else console.log(`A palavra ${palavra} não é um palíndromo !`);
 
 fdffffdfd
-fdfdffdfdffdfdfds
+fdfdffdfdffdfdfdfds
 fdfdf
 fd
